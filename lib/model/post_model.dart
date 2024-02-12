267f7c68a1509005de43a40088bc7a6171d78d74
ToshevaDurdona <toshevadurdona2002@gmail.com>
2024-02-12 16:19:06 +0500

    instagram

---
+++ lib/model/post_model.dart
@@ -0,0 +1,12 @@
+class Post{
+  String? userImage;
+  String? userName;
+  String? postImage;
+  String? caption;
+  String? userImage1;
+  String? userName1;
+  String? postImage1;
+  String? caption1;
+
+  Post({this.userImage,this.userName,this.postImage,this.caption,this.userImage1,this.userName1,this.postImage1,this.caption1});
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ngdemo5/pages/feed_page.dart';
+import 'package:ngdemo5/services/colors_service.dart';
 
 class HomePage extends StatefulWidget {
   static const String id = "home_page";
@@ -10,20 +12,63 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var _pages=[
+    FeedPage(),
+    FeedPage(),
+    FeedPage(),
+    FeedPage(),
+    FeedPage(),
+  ];
+
+ int currentPage=0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.black,
         //backgroundColor: Color.fromRGBO(0, 127, 255, 1),
-        //backgroundColor: HexColor("#007FFF"),
+        // backgroundColor: HexColor("#007FFF"),
+        // android ba iosda bir xil ochilishi uchun
+        centerTitle: true,
+        elevation: 0,
         title: Text(
           "Instagram",
-          style: TextStyle(color: Colors.white, fontSize: 30, fontFamily: "Billabong.ttf"),
+          style: TextStyle(color: Colors.grey),
         ),
+        // chapdan yaratib beradi
+        leading: IconButton(
+          onPressed: (){},
+          icon: Icon(Icons.camera_alt_outlined,color: Colors.grey,),
+        ),
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.tv,color: Colors.grey,),
+          ), IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.send_sharp,color: Colors.grey,),
+          ),
+        ],
       ),
-      body: Center(
-        child: Text("Welcome to Instagram"),
+      body: _pages[currentPage],
+      bottomNavigationBar:BottomNavigationBar(
+        currentIndex:currentPage,
+        onTap:(i){
+          setState(() {
+            currentPage=i;
+          });
+        },
+        type:BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.black,
+        items: [
+          BottomNavigationBarItem(icon:Icon (Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon:Icon (Icons.search),label:"Search" ),
+          BottomNavigationBarItem(icon:Icon (Icons.add_box_outlined),label: "Upload"),
+          BottomNavigationBarItem(icon:Icon (Icons.favorite_border),label:"Likes" ),
+          BottomNavigationBarItem(icon:Icon (Icons.person),label:"Profile" ),
+        ],
       ),
     );
   }
